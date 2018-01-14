6e50f52913dc4b0d9061127907dc870fc7eae2c4
shimilysj <124954495@qq.com>
2018-01-14 18:40:50 +0800

    添加事务功能

--- src/main/java/com/winter/Controller/UserController.java
+++ src/main/java/com/winter/Controller/UserController.java
@@ -3,6 +3,7 @@ package com.winter.Controller;
 import com.winter.model.User;
 import com.winter.service.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -30,5 +31,19 @@ public class UserController {
     public Object findAllUser(@PathVariable("pageNum") int pageNum, @PathVariable("pageSize") int pageSize){
         List<User> allUser = userService.findAllUser(pageNum, pageSize);
         return allUser;
+    }
+
+
+    /**
+     * 事务
+     * @param user
+     * @return
+     */
+    @Transactional
+    @RequestMapping(value = "/transtration")
+    public String transtration(User user){
+        int insert = userService.insert(user);
+        int i = 1/0;
+        return "success";
     }
 }
